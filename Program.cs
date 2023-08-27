e5614246e461e570c7647764c6f2c69902eb3158
Addison Roy <74374312+mama-sucker@users.noreply.github.com>
2023-08-27 00:21:18 -0400

    code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Module 1: Excersise 1.1 
+// Unit Test #1
+Console.WriteLine("Lucinda Potter");
+Console.WriteLine("6/24/1992");
+Console.WriteLine("work 000-000-0101");
+Console.WriteLine("cell 000-000-0189");
+// Unit Test #2
+Console.WriteLine("\nJohn Smith");
+Console.WriteLine("8/15/1978");
+Console.WriteLine("work 000-111-0101");
+Console.WriteLine("cell 000-111-0189");
+// Unit Test #3
+Console.WriteLine("\nChris Johnson");
+Console.WriteLine("12/01/1987");
+Console.WriteLine("work 000-222-0101");
+Console.WriteLine("cell 000-222-0189");
+// Unit Test #4
+Console.WriteLine("\nRobert Hall");
+Console.WriteLine("2/27/1949");
+Console.WriteLine("work 000-444-0101");
+Console.WriteLine("cell 000-444-0189");
